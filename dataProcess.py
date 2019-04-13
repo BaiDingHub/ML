6c2f28efcc5039a15bf9e2cadc053af25aa7d26e
yz <Zachary@yu.com>
2019-04-13 11:57:11 +0800

    evalute

--- dataProcess.py
+++ dataProcess.py
@@ -56,6 +56,9 @@ def getSeveralAtt(datasets,str,num):
 
 
 def transOnehot(dataSet,index):
+    '''
+    将数据转换成Onehot类型
+    '''
     length = len(dataSet)
     max = dataSet[index].max().astype(int)
     result = []
@@ -64,4 +67,66 @@ def transOnehot(dataSet,index):
         zero[int(number)] = 1
         result.append(zero.tolist())
     return np.array(result)
+
+
+# In[ ]:
+
+
+#留出法
+def hold_out(dataSet,train_size):
+    """
+    dataSet:数据集
+    train_size:留出法中训练集所占得比例
+    """
+    totalLen = dataSet.shape[0]
+    len = int(totalLen * train_size)
+    index = range(totalLen)
+    index_1 = np.random.choice(index,len,replace=False)     #训练集的下标
+    index_2 = np.delete(index,index_1)                      #测试集的下标
+    train = dataSet[index_1]
+    test = dataSet[index_2]
+    return train,test
+
+
+# In[ ]:
+
+
+#交叉验证法
+def cross_valid(dataSet,k=10):
+    """
+    dataSet:数据集
+    k：k次交叉验证的数目
+    return : datasets：shape(k,num,feature)  list类型
+    """
+    datasets =[]
+    num = int(dataSet.shape[0]/k)
+    start = 0
+    end = num
+    for i in range(k):
+        datasets.append(dataSet[start:end,:].tolist())
+        start += num
+        end += num
+    return datasets
+
+
+# In[ ]:
+
+
+#自助法
+def bootstrapping(dataSet):
+    m = dataSet.shape[0]
+    index1 = []
+    index2 = []
+    for i in range(m):
+        index1.append(np.random.randint(m))
+    index2 = np.delete(range(m),index1)
+    train = dataSet[index1]
+    test = dataSet[index2]
+    return train,test
+
+
+# In[ ]:
+
+
+
 
